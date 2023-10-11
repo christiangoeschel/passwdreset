5ff76878203896b63c4310b1e2e6d7345be366ee
Christian Goeschel Ndjomouo <124460207+christiangoeschel@users.noreply.github.com>
2023-10-11 01:41:08 -0400

    Update passwordreset.sh

--- passwordreset.sh
+++ passwordreset.sh
@@ -1,97 +1,103 @@
 #!/bin/bash
 #
-#Password reset script to automate the main partition identification and mounting
-#for the password update/reset
+# [ Name ]:             Password Reset Automation 
+# [ Version ]:          1.2
+# [ Author ]:           Christian Goeschel Ndjomouo
+# [ Created on ]:       Sep 17 2023
+# [ Last updated on ]:  Oct 11 2023
+# [ Tested on ]:        All Linux distribution images offered by OVHcloud ( VPS and Dedicated server )
 #
-#Author: Christian Goeschel Ndjomouo
-#Version:1.1
+# [ Description ]:      This program takes a random IPv4 address, subnet mask in DDN or CIDR as input.
+#                       
+#                       
+#              
 
-#############################################
-#Start of script output
+#   Start of script output
 echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "|                                                                                  |"
-echo "|  This script will handle the identification and mounting of your main partition  |"
-echo "|  and help you update/reset your forgotten password.                              |"
-echo "|                                                                                  |"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+------------------------------------------------------------------------------------+"
+echo "|                                                                                    |"
+echo "|  This program will handle the identification and mounting of your main partition   |"
+echo "|  to then change the root directory in order to help you reset your lost password.  |"
+echo "|                                                                                    |"
+echo "+------------------------------------------------------------------------------------+"
 echo ""
-echo "Starting..."
+echo "Starting program now ..."
 sleep 2
 
-##########################################################################################
-#Tune2fs package availability check, this tool will help to identify the main partition
 
-t2fs=$(which tune2fs)
+#   Tune2fs package availability check, this tool will help to identify the main partition
+t2fs_path=$(which tune2fs)  # Checking whether 'tune2fs' is found in any of the in the $PATH variable specified directories
 echo ""
-echo "Checking for tune2fs..."
+echo "Checking for tune2fs ..."
 sleep 1
 
-if [[ $t2fs == *"tune2fs"* ]];
-then
-
-echo ""
-echo "tune2fs already installed!"
+#   If the 't2fs_path' variable value includes the string 'bin' it is safe to say that tune2fs binary has been installed
+if [[ $t2fs_path == *"bin"* ]];
+    then
+    echo ""
+    echo "tune2fs already installed!"
 
 else
-
-echo ""
-echo "Installing tune2fs..."
-apt install e2fslibs -y
-sleep 1
+    echo ""
+    echo "Installing tune2fs..."
+    apt install e2fslibs -y
+    sleep 1
 
 fi
 
 
-#############################################
-#Server type identification
 
-
-
-ip=$(ip -o a | grep -E 'ens3.*inet|eth0.*inet' | grep -v 'inet6' | cut -d '/' -f 1 | cut -d ' ' -f 7)
+#   Server type identification
+#   Here we will identify the server type via the hostname which we will receive from the 'uname -n' command
 server_name=$(uname -n)
 server_type=""
 
+#   The for loop runs 3 times and gives the user 3 chances to type in the server type manually.
+#   This would only happen if the 'uname -n' output was not enough to determine the server type.
+for a in `seq 1 3`;
+do      
 
-for a in `seq 1 4`;
-do
-
-        if [[ $a == "3" ]];
+        # If user reaches his third attempt the program will exit and terminate
+        if [ $a == "3" ];     
         then
 
-        echo "Too many ambigious inputs. Stopping script ..."
+        echo -e "\nToo many ambigious inputs. Stopping program ..."
         sleep 2
         exit 0
 
-        elif [[ $server_name == *"vps"* ]];
+        elif [ $server_name == *"vps"* ];
         then
 
-        server_type="VPS"
+        server_type="V"
         break
 
-        elif [[ $server_name == *"ns"* ]];
+        elif [ $server_name == *"ns"* ];
         then
 
-        server_type="Dedicated Server"
+        server_type="D"
         break
 
+        #   This block would be executed if the 'uname -n' output was not sufficient to determine the server type
         else
 
-        echo -e "\nPlease indicated whether your server is a VPS ( V ) or a Dedicated server ( D ) with the respective character:"
+        echo -e "\nPlease indicated whether your server is a VPS ( V ) \nor a Dedicated server ( D ) with the respective character:"
         read server_type
 
-                if [[ $server_type == "V" ]];
+                #   User input was V or v so the server type is set to V for (VPS)
+                if [ $server_type == "V" ] || [ $server_type == "v" ];
                 then
 
-                server_type="VPS"
+                server_type="V"
                 break
 
-                elif [[ $server_type == "D" ]];
+                #   User input was D or d so the server type is set to D for (Dedicated server) 
+                elif [ $server_type == "D" ] || [ $server_type == "d" ];
                 then
 
-                server_type="Dedicated Server"
+                server_type="D"
                 break
 
+                #   If none of the above options ( V,v or D,d ) was entered the loop will continue to the next iteration
                 else
                 continue
 
@@ -99,10 +105,11 @@ do
         fi
 done
 
-#############################################
-#Server type dependent if statement
 
-if [[ $server_type == "VPS" ]];
+#   This flow control depends on the server type
+#
+#   Code block will be executed if the server is a dedicated server
+if [ $server_type == "V" ];
 then
 
     #############################################
@@ -113,135 +120,124 @@ then
 
     pot_part=$(lsblk | grep -E 'sd|nv' | grep 'part' | grep 'G' | cut -d ' ' -f 1 | tr -cd '[a-zA-Z.\n.1-9]')
 
-    for partitions in $(echo $pot_part);
+    for partition in $(echo $pot_part);
     do
 
-            part_size=$(lsblk -l /dev/$partitions | grep "G" | cut -d "G" -f 1 | cut -d "0" -f 2 | tr -cd '[0-9..]')
+            part_size=$(lsblk -l /dev/$partition | grep "G" | cut -d "G" -f 1 | cut -d "0" -f 2 | tr -cd '[0-9..]')
 
-            if [[ $part_size > $biggest_prt_size ]];
+            if [ $part_size > $biggest_prt_size ];
             then
 
                     biggest_prt_size=$part_size
-                    biggest_prt=$partitions
+                    biggest_prt=$partition
 
             fi
     done
 
-            echo ""
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "Main partition detected! Mounting /dev/$partitions to /mnt/$partitions ..."
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+            echo -e "\nMain partition detected! \nMounting /dev/$partition to /mnt/$partition ..."
 
-            mkdir -p /mnt/$partitions
-            mount /dev/$partitions /mnt/$partitions
-            echo ""
-            echo "MOUNTED!"
-            echo ""
-            sleep 1
+            #   Creating the mount point directory
+            mkdir -p /mnt/$partition
+            #   Mounting the main partition to the mount point directory
+            mount /dev/$partition /mnt/$partition
+            
+            echo -e "\nMain partition $partition has been mounted successfully !!!"
+            echo "Analyzing file system and determining root directory ..." 
+            sleep 2
 
-
-elif [[ $server_type == "Dedicated Server" ]];
+#   Code block will be executed if the server is a dedicated server
+elif [[ $server_type == "D" ]];
 then
 
-    #############################################
-    #Dedicated server partition detection
+    
+    #   Dedicated server partition detection
 
-    pot_part=$(lsblk | grep -E 'sd|nv' | grep 'part' | cut -d ' ' -f 1 | tr -cd '[.a-zA-Z.\n.0-9]')
-    dsk_rslts=$( echo $pot_part | wc -l )
-
-    for partitions in $(echo $pot_part);
+    #  This variable 
+    potential_part=$(lsblk | grep -E 'sd|nv' | grep 'part' | cut -d ' ' -f 1 | tr -cd '[.a-zA-Z.\n.0-9]')
+    
+    #   The for loop will run through all the partitions listed in the potential_part variable
+    for partition in $(echo $potential_part);
     do
 
-    mntpnt=$(tune2fs -l /dev/$partitions | grep 'mounted' | cut -d ":" -f 2 )
+    #   tune2fs will print the partitions information and it's last mount point
+    #   the partition that was mounted at "/" will qualify as the main partition
+    mounted_at=$(tune2fs -l /dev/$partition | grep 'mounted' | cut -d ":" -f 2 )
 
-
-        if [[ $(echo $mntpnt) == "/" ]] || [[ $(echo $mntpnt) == *"/mnt"* ]];
+        #   If the currently selected partition's last mount point was "/" 
+        #   it will qualify as the main partition
+        if [ $(echo $mounted_at) == "/" ] || [ $(echo $mounted_at) == *"/mnt"* ];
         then
-
-            echo ""
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "Main partition detected! Mounting /dev/$partitions to /mnt/$partitions ..."
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-
-            mkdir -p /mnt/$partitions
-            mount /dev/$partitions /mnt/$partitions
-            echo ""
-            echo "MOUNTED!"
-            echo ""
-            sleep 1
+            echo -e "\nMain partition detected! \nMounting /dev/$partition to /mnt/$partition ..."
+            #   Creating the mount point directory
+            mkdir -p /mnt/$partition
+            #   Mounting the main partition to the mount point directory
+            mount /dev/$partition /mnt/$partition
+            
+            echo -e "\nMain partition $partition has been mounted successfully !!!"
+            echo "Analyzing file system and determining root directory ..." 
+            sleep 2
             break
-
+        
+        #   If the currently selected partition could not be identified as the main partition
+        #   the loop will continue with the next iteration
         else
             continue
 
         fi
     done
 
-
+#   Code block will be executed if the server type could not have been determined
+#   The program will have to prompt a message to the user to manually identify the main partition
 else
 
-        #############################################
-        #Main partition detection failure
+        #Main partition detection failure message
+        echo -e "\n[ ERROR ]"
+        echo "Unfortunately, we could not detect the main partition."
+        echo "Please have a look at the partition list down below and determine which one it is."
+        echo -e "\nOnce you have located the main partition, enter it in the prompt down below.\n"
 
-        echo "                                          FAILURE                                        "
-                echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-                echo "|                                                                                       |"
-                echo "|  Unfortunately, we could not detect the main partition.                               |"
-                echo "|  Please have a look at the partition list down below and determine which one it is.   |"
-                echo "|                                                                                       |"
-                echo "|  Once you have located the main partition, enter it in the prompt down below.         |"
-                echo "|                                                                                       |"
-                echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-
-
-                lsblk
-                sleep 2
-        echo ""
-        echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-                echo "Please type in the partition name:"
-        read $partitions
-
-        echo ""
-        echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        echo "Main partition detected! Mounting /dev/$partitions to /mnt/$partitions ..."
-        echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-
-        mkdir -p /mnt/$partitions
-        mount /dev/$partitions /mnt/$partitions
-        echo ""
-        echo "MOUNTED!"
-        echo ""
-        sleep 1
-
+        #   Printing the block devices detected by the kernel
+        lsblk
+        
+        sleep 2
+        echo "Please type in the partition name ( sdXx or nvmeXnXpX ):"
+        read $partition
+        echo -e "\n[ SUCCESS ]"
+        echo -e "\nMain partition detected! \nMounting /dev/$partition to /mnt/$partition ..."
+        #   Creating the mount point directory
+        mkdir -p /mnt/$partition
+        #   Mounting the main partition to the mount point directory
+        mount /dev/$partition /mnt/$partition
+    
+        echo -e "\nMain partition $partition has been mounted successfully !!!"
+        echo "Analyzing file system and determining root directory ..." 
+        sleep 2
 
 fi
 
 
-
-#############################################
-#Username validation
-
-echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "Please type in the username for which you want to change the password:"
+#   Username validation
+#
+#   The customer is asked to type in their username in order to change the
+#   correct user accounts password
+echo -e "\n\nPlease type in the username for which you want to change the password\n"
+echo "It is NOT 'root' it is the username you have received after your last installation via email."
+echo "Type in username:"
 read username
 
-#The customer is asked to type in their username in order to change the
-#correct user accounts password
-#This for loop runs 2 times which gives the customer a total of three attempts
 
+#   This for loop gives the customer a total of three attempts
 for attempt in `seq 1 3`;
 do
 
-#Checks whether the typed in username is in the /etc/passwd file
-        passwd_file_dir="$(find /mnt/$partitions/ -type f -name 'passwd' | grep -E 'etc/passwd$')"
+#   Checks whether the typed in username is in the /etc/passwd file
+        passwd_file_dir="$(find /mnt/$partition/ -type f -name 'passwd' | grep -E 'etc/passwd$')"
         username_found="$(cat $passwd_file_dir | grep $username | cut -d ':' -f 1)"
         
         if [ "$username" == "$username_found" ];
         then
 
-                echo ""
-                echo "Thank you!"
+                echo "[ SUCCESS ]"
                 break 2
 
         elif [ "$attempt" == "3" ];
@@ -259,16 +255,15 @@ do
                 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
                 #Unmounting the main partition from the mount point
-                echo "Unmounting /mnt/"$partitions" ..."
+                echo "Unmounting /mnt/"$partition" ..."
                 sleep 2
-                umount /mnt/$partitions
+                umount /mnt/$partition
 
                 #Stopping the script
                 exit 1
 
         else
-
-                echo ""
+                echo "[ ERROR ]"
                 echo "The username could not be found!"
 
                 #Outputs the amount of attempts that are left
@@ -280,39 +275,35 @@ do
 
 done
 
-#############################################
-#Password change announcement
 
+#   Password change announcement
 echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "You will now be asked to enter a new password and re-enter it."
-echo "Please make sure to remember your password."
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e "Please make sure to remember your password.\n"
+sleep 3
 
-sleep 1
 
-#############################################
-#Chroot into mount point
-chroot_dir="$(find /mnt/$partitions/ -type f -name 'passwd' | grep -E 'etc/passwd$' | sed 's/etc\/passwd//')"
+#   Changing root directory to the main partition
+#
+#   This pattern template will filter out the root directory in the main partition
+chroot_dir="$(find /mnt/$partition/ -type f -name 'passwd' | grep -E 'etc/passwd$' | sed 's/etc\/passwd//')"
+#   Chanhing the root directory
+echo "Changing the root directory to $chroot_dir"
+#   Evoking the passwd binary with the username variable as argument in the new root directory
 chroot $chroot_dir passwd $username
 echo ""
-echo ""
 sleep 1
 
-##########################################################
-#Unmounting the main partition from the mount point
-echo "Unmounting /mnt/"$partitions" ..."
+
+#   Unmounting the main partition from the mount point
+echo "Unmounting /mnt/"$partition" ..."
 sleep 1
-umount /mnt/$partitions
+umount /mnt/$partition
 
 #############################################
-#Final success message
-echo ""
-echo "                                          SUCCESS                                        "
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "|                                                                                       |"
-echo "|  YOUR PASSWORD HAS SUCCESSFULLY BEEN UPDATED!                                         |"
-echo "|  You can now reboot your server from hard drive in the OVHcloud control panel.        |"
-echo "|  If you need further assistance feel free to contact the OVHcloud technical support.  |"
-echo "|                                                                                       |"
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+#   Final success message
+echo -e "!!! [ SUCCESS ] !!!\n"
+echo -e "YOUR PASSWORD FOR $username HAS BEEN UPDATED !\n"
+echo "You can now reboot your server from the usual disk in the OVHcloud control panel."       
+echo "If you need further assistance feel free to contact the OVHcloud technical support."
+
